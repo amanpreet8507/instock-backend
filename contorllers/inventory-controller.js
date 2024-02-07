c7ede187813f4fc1e29f3386302db22554ac7320
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-02-07 15:01:54 -0800

    API to GET a Single Inventory Item

--- contorllers/inventory-controller.js
+++ contorllers/inventory-controller.js
@@ -1,12 +1,16 @@
 const knex = require("knex")(require("../knexfile"));
 
-// FOR ----> GET /api/warehouses/:id/inventories
-const inventories = async (req, res) => {
+// FOR -----> GET /api/warehouses/:id/inventories ******************************************
+// FOR -----> API to GET Inventories for a Given Warehouse
+
+const allInventories = async (req, res) => {
   try {
+    // Fetch the whole inventories array with all keys and properties
     const inventoriesArrWithAllKeys = await knex("warehouses")
       .join("inventories", "inventories.warehouse_id", "warehouses.id")
       .where({ warehouse_id: req.params.id });
 
+    // Filter our keys and properties that are required and return that new array
     const inventoriesArrWithItemKeys = inventoriesArrWithAllKeys.map(
       (item) => ({
         id: item.id,
@@ -16,14 +20,45 @@ const inventories = async (req, res) => {
         quantity: item.quantity,
       })
     );
-    res.json(inventoriesArrWithItemKeys);
+    res.status(200).json(inventoriesArrWithItemKeys);
   } catch (error) {
-    res.status(500).json({
-      message: `Unable to retrieve inventories for warehouse with ID ${req.params.id}: ${error}`,
+    res.status(404).json({
+      message: `Unable to retrieve inventories for warehouse with ID ${req.params.id}: ${error}!`,
+    });
+  }
+};
+
+
+// For -----> GET /api/inventories/:id ************************************************************
+// FOR -----> API to GET a Single Inventory Item
+
+const inventoriesById = async (req, res) => {
+  try {
+    const inventoryFound = await knex("inventories").where({
+      id: req.params.id,
+    });
+
+    // If there is no inventory found
+    if (inventoryFound.length === 0) {
+      return res.status(400).json({
+        message: `Inventory with ID${req.params.id} not found!`,
+      });
+    }
+    // Return the first object found
+    const inventoryObject = inventoryFound[0];
+
+    // Remove created_at and updated_at keys and properties and return that new Object
+    const { created_at, updated_at, ...filteredInventoryObject } =
+      inventoryObject;
+    res.status(200).json(filteredInventoryObject);
+  } catch (error) {
+    res.status(404).json({
+      message: `Unable to retrieve inventory with ID ${req.params.id}: ${error}!`,
     });
   }
 };
 
 module.exports = {
-  inventories,
+  allInventories,
+  inventoriesById,
 };
--- routes/inventory-route.js
+++ routes/inventory-route.js
@@ -1,6 +1,11 @@
 const router = require("express").Router();
 const inventoryController = require("../contorllers/inventory-controller");
 
-router.route("/:id/inventories").get(inventoryController.inventories);
+// FOR -----> GET /api/warehouses/:id/inventories
+router.route("/:id/inventories")
+    .get(inventoryController.allInventories);
 
+// For -----> GET /api/inventories/:id
+router.route("/:id")
+    .get(inventoryController.inventoriesById)
 module.exports = router;
