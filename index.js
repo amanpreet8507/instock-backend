1654b19de8ef7e17a504cc6433ea8423ee929b15
leafstash <amanpreetk1@leafstash.com>
2025-04-18 17:09:57 -0700

    DB version

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.MYSQLPORT || 5050;
 
 // Use routes
 const inventoryRoutes = require("./routes/inventory-route");
--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const mysql = require("mysql2");
-const urlDB = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_PASSWORD}`;
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
 
 const connection = mysql.createConnection(urlDB);
 
